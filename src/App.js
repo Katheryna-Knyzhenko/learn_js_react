9902f5da94e0b69988984ab260af150b35359c70
Kate <evgen.knyzhenko@gmail.com>
2020-06-06 00:12:13 +0300

    drag

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  (function dragDrop() {
-    const dragEl = document.querySelector('.drag');
-    dragEl.draggable = true;
-  }());
-  return (
-    <div className="App"> Я Drag Drop пример
-      <div className='drag'>Перетащи меня! Ну же!</div>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    function dragDrop() {
+      const dragEl = document.querySelector('.drag');
+      dragEl.draggable = true;
+    }
+    dragDrop();
+    return (
+        <div className="App"> Я Drag Drop пример
+          <div className='drag' >Перетащи меня! Ну же!</div>
+        </div>
+    );
+  }
 }
 
 export default App;
