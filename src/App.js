397c5fd5aaaf7fd7df59f0f570f51fed19682d54
Kate <evgen.knyzhenko@gmail.com>
2020-06-05 23:36:19 +0300

    drag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  (function dragDrop() {
+    const dragEl = document.querySelector('.drag');
+    dragEl.draggable = true;
+  }());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> Я Drag Drop пример
+      <div className='drag'>Перетащи меня! Ну же!</div>
     </div>
   );
 }
