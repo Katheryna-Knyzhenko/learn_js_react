2f867950e9594c0da354dc1e530929fd906a6a10
Kate <evgen.knyzhenko@gmail.com>
2020-06-06 14:29:25 +0300

    dropEnd

--- src/App.js
+++ src/App.js
@@ -11,9 +11,35 @@ componentDidMount() {
         const card = document.querySelector('.js-card');
         const cells = document.querySelectorAll('.js_cell');
         const dragStart = function () {
-            console.log('dragStart');
+            setTimeout(() => { this.classList.add('hide')}, 0)
+
         };
+        const dragEnd = function () {
+            setTimeout(() => { this.classList.remove('hide')}, 0)
+        };
+        const dragOver = function (event) {
+            event.preventDefault();
+        };
+        const dragEnter = function (event) {
+            event.preventDefault();
+            this.classList.add('hover');
+        };
+        const dragLeave = function () {
+            this.classList.remove('hover');
+        };
+        const dragDrop = function () {
+            this.append(card);
+            this.classList.remove('hover');
+        };
+        cells.forEach((cell) => {cell.addEventListener('dragover', dragOver);
+            cell.addEventListener('dragenter', dragEnter);
+            cell.addEventListener('dragleave', dragLeave);
+            cell.addEventListener('drop', dragDrop);
+        });
+
         card.addEventListener('dragstart', dragStart);
+        card.addEventListener('dragend', dragEnd);
+
     };
     dragDrop();
 }
