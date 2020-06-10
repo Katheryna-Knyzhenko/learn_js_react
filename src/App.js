d4109a6b215a241b37e7e808837bba0f3b0e806f
Kate <evgen.knyzhenko@gmail.com>
2020-06-10 15:27:46 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -11,16 +11,17 @@ componentDidMount() {
     const resultBlock = document.querySelector('#result');
     const  clickMeButton = document.querySelector('#clickMe');
     const  pageNumber = document.querySelector('#page-number');
-    clickMeButton.addEventListener('click', makeRequest);
-    function makeRequest () {
-        $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-            success: function (data) {
-                data.forEach(el => {
+    clickMeButton.addEventListener('click', () => {getImages(pageNumber.value, onDataRecieved)});
+    function onDataRecieved (data) {
+        data.forEach(el => {
                     const img = document.createElement('img');
                     img.src = el.thumbnail;
                     document.querySelector("#result").appendChild(img);
                 })
-            }
+    }
+    function getImages (page, successCallback) {
+        $.ajax(`https://repetitora.net/api/JS/Images?page=${page}&count=1`, {
+            success: successCallback
         });
     }
     const dragDrop = () => {
